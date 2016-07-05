45968604a64748862f93c41f2c3f62cb63810894
kazakevic <darjus.kazakevic@gmail.com>
2016-07-05 17:50:27 +0300

    add
    add

---
+++ search.php
@@ -0,0 +1,41 @@
+<?php
+//search("Sviecia saule", grybas) ===> false
+//search("Sviecia saule", sau le) ===> true
+
+function check($part, $str)
+{
+
+	$word = "";
+	$words[] = "";
+
+		$n = 0;
+		for($i=0; $i < strlen($str); $i++)
+		{
+
+			if($str[$i] == " ")
+			{
+				$n += 1;
+				$word = "";
+			}
+			if($str[$i] != " ")
+			{
+				$word .=$str[$i];
+				$words[$n] = $word;
+			}
+			
+		}
+
+		foreach ($words as $word) {
+			if($word == $part)
+				return true;
+			else
+				return false;
+		}
+}
+
+		if(check("Labasss", "Labas ka tu"))
+			echo "labas";
+		else
+			echo "atia";
+
+?>
